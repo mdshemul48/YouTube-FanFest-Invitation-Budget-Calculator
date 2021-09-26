c98190af56b9b10e8f63e77302ec6bd8b369829b
MD Shimul <mdshemul480@gmail.com>
2021-09-26 11:51:12 +0600

    adding header area

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import "./Header.css";
+// header component with just text
 const Header = () => {
   return (
     <header>
       <div>
-        <h1>welcome to our site</h1>
+        <h1 className='main-title'>YouTube FanFest 2021 Invitation</h1>
+        <h3 className='secondary-title'>Total budget 1M USD</h3>
       </div>
     </header>
   );
