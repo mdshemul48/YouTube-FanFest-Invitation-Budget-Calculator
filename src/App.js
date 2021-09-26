b9095d7ae6acff4df8e87aefb024bb50593b99db
MD Shimul <mdshemul480@gmail.com>
2021-09-26 11:29:42 +0600

    adding font and creating header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className='App'>
-      <h1>hello world</h1>
-    </div>
+    <>
+      <Header></Header>
+    </>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <Header>this is header</Header>;
+};
+
+export default Header;
