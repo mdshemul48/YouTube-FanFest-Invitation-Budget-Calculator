8ba32b4703b0a0d074a4fb0daf56115302e6c616
MD Shimul <mdshemul480@gmail.com>
2021-09-26 13:45:58 +0600

    adding new users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Guests from "./components/Guests/Guests";
+import TotalCostCard from "./components/TotalCost/TotalCostCard";
+
+import "./App.css";
 function App() {
   return (
     <>
       <Header></Header>
-      <Guests></Guests>
+      <main className='main-container'>
+        <Guests></Guests>
+
+        <TotalCostCard></TotalCostCard>
+      </main>
     </>
   );
 }
---
+++ src/components/TotalCost/TotalCostCard.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./TotalCostCard.css";
+const TotalCostCard = () => {
+  return (
+    <div className='cost-card'>
+      <h2>YouTuber Invited: 5</h2>
+      <h2>Total Cost: 100000</h2>
+      <hr className='line' />
+
+      <h3>VanossGaming</h3>
+      <h3>VanossGaming</h3>
+      <h3>VanossGaming</h3>
+      <h3>VanossGaming</h3>
+    </div>
+  );
+};
+
+export default TotalCostCard;
